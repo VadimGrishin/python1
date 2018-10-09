ca0397cf84306a5eaef1997a4f2915b5a92307c3
Vadim <kran1@inbox.ru>
2018-10-09 21:48:02 +0300

    ДЗ 4 normal

--- hw04_normal.py
+++ hw04_normal.py
@@ -19,7 +19,38 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+print('\nЗадание-1' + '-'*20)
+res = []
+process = 1 # 0 - lower, 1 - upper
+el=''
+for ch in line:
+    if process == 0:
+        if ch.lower() == ch:
+            el +=ch
+        else:
+            res.append(el)
+            el = ''
+            process = 1
+    else:
+        if ch.lower() == ch:
+            el = ch
+            process = 0
+        else:
+            pass
+if process == 0:
+    res.append(el)
 
+print('res=', res)
+print('\n')
+
+import re
+pattern = '[a-z]+'
+
+res2 = re.findall(pattern, line)
+print('res2=', res2)
+
+diff = [x for x in res if x not in res2] #контрольная разница результатов
+print('diff=', diff)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -44,6 +75,10 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+print('\nЗадание-2' + '-'*20)
+pattern = '[a-z]{2}([A-Z]+)[A-Z]{2}'
+res2 = re.findall(pattern, line_2)
+print('res2=', res2)
 
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
@@ -51,3 +86,45 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+print('Задание-3'+'-'*20)
+import os, shutil
+from random import randint
+
+if not os.path.exists('res.txt'):
+    with open('res.txt', 'w', encoding='utf-8') as g:
+            g.writelines("%s" % i for i in [randint(0, 9) for x in range(2500)])
+
+# 1-й способ (за один проход)
+with open('res.txt', 'r', encoding='utf-8') as g:
+    s = g.read()
+    prev=''
+    n = 1 #текущая длина
+    mx = 1 #максимальная длина
+    for ch in s:
+        if ch == prev:
+            n +=1
+        else:
+            if mx <= n:
+                mx = n
+                mxdigit = prev
+            prev = ch
+            n = 1
+print('Максимум - ', mx, 'Строка - ', mxdigit*mx)
+
+# 2-й способ (регулярные выражения)
+mx = 1
+for ch in map(str,range(0,10)):
+    n=1
+    pattern = ch
+    while re.findall(pattern, s):
+        pattern += ch
+        n +=1
+    n -= 1
+    print('найдена подстрока - ', pattern[:-1]) 
+    if mx <= n:
+        mx = n
+        mxdigit = ch
+print('Максимум - ', mx, 'Строка - ', mxdigit*mx)   
+    
+
+
